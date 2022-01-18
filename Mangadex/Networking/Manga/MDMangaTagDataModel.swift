3eb13a29db1034b6402247625206571756cbe7ce
Sokiraon <51328494+Sokiraon@users.noreply.github.com>
2022-01-18 18:34:45 +0800

    feat(localize): add full localization for manga tags

--- Mangadex/Networking/Manga/MDMangaTagDataModel.swift
+++ Mangadex/Networking/Manga/MDMangaTagDataModel.swift
@@ -9,15 +9,7 @@ class MDMangaTagAttributes: NSObject {
     @objc var name: MDMangaMultiLanguageObject!
     
     func localizedName() -> String {
-        let original = name.localizedString()
-        guard let key = MDMangaTagReflecter[original] else {
-            return original
-        }
-        let localized = key.localized()
-        if (localized == key) {
-            return original
-        }
-        return localized
+        return name.en?.localized() ?? ""
     }
 }
 
@@ -25,28 +17,3 @@ class MDMangaTagDataModel: NSObject {
     @objc var id: String!
     @objc var attributes: MDMangaTagAttributes!
 }
-
-fileprivate let MDMangaTagReflecter = [
-    "Reincarnation": "kTagReincarnation",
-    "Action": "kTagAction",
-    "Demons": "kTagDemons",
-    "Comedy": "kTagComedy",
-    "Martial Arts": "kTagMartial",
-    "Magic": "kTagMagic",
-    "Harem": "kTagHarem",
-    "Isekai": "kTagIsekai",
-    "Drama": "kTagDrama",
-    "School Life": "kTagSchool",
-    "Fantasy": "kTagFantasy",
-    "Romance": "kTagRomance",
-    "Slice of Life": "kTagLife",
-    "Award Winning": "kTagAward",
-    "Adaptation": "kTagAdaptation",
-    "Long Strip": "kTagLongStrip",
-    "Monsters": "kTagMonsters",
-    "Samurai": "kTagSamurai",
-    "Sci-Fi": "kTagSciFi",
-    "Superhero": "kTagSuperhero",
-    "Gore": "kTagGore",
-    "Supernatural": "kTagSupernatural",
-]
